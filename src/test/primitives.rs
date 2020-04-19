fcad756d67b5ddd31284b26f2bd7a50f442424e1
zing <599490911@qq.com>
2020-04-19 15:26:14 +0800

    update primitives

---
+++ src/test/primitives.rs
@@ -0,0 +1,145 @@
+#[test]
+fn primitives() {
+    // Variables can be type annotated.
+    let _logical: bool = true;
+
+    let _a_float: f64 = 1.0;  // Regular annotation
+    let _an_integer = 5i32; // Suffix annotation
+
+    // Or a default will be used.
+    let _default_float = 3.0; // `f64`
+    let _default_integer = 7;   // `i32`
+
+    // A type can also be inferred from context
+    let mut inferred_type = 12; // Type i64 is inferred from another line
+    inferred_type = 4294967296i64;
+
+    // A mutable variable's value can be changed.
+    let mut mutable = 12; // Mutable `i32`
+    mutable = 21;
+
+    // Error! The type of a variable can't be changed.
+    //mutable = true;
+
+    // Variables can be overwritten with shadowing.
+    let _mutable = true;
+}
+
+#[test]
+fn print() {
+    // Integer addition
+    println!("1 + 2 = {}", 1u32 + 2);
+    // Integer subtraction
+    println!("1 - 2 = {}", 1i32 - 2);
+
+    // Short-circuiting boolean logic
+    println!("true AND false is {}", true && false);
+    println!("true OR false is {}", true || false);
+    println!("NOT true is {}", !true);
+
+    // Bitwise operations
+    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
+    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
+    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
+    println!("1 << 5 is {}", 1u32 << 5);
+    println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
+
+    // Use underscores to improve readability!
+    println!("One million is written as {}", 1_000_000u32);
+}
+
+#[test]
+fn tuples() {
+    fn reverse(pair: (i32, bool)) -> (bool, i32) {
+        // `let` can be used to bind the members of a tuple to variables
+        let (integer, boolean) = pair;
+
+        (boolean, integer)
+    }
+
+    // The following struct is for the activity.
+    #[derive(Debug)]
+    struct Matrix(f32, f32, f32, f32);
+
+
+    // A tuple with a bunch of different types
+    let long_tuple = (1u8, 2u16, 3u32, 4u64,
+                      -1i8, -2i16, -3i32, -4i64,
+                      0.1f32, 0.2f64,
+                      'a', true);
+
+    // Values can be extracted from the tuple using tuple indexing
+    println!("long tuple first value: {}", long_tuple.0);
+    println!("long tuple second value: {}", long_tuple.1);
+
+    // Tuples can be tuple members
+    let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
+
+    // Tuples are printable
+    println!("tuple of tuples: {:?}", tuple_of_tuples);
+
+    // But long Tuples cannot be printed except length 12
+    let too_long_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, /*13*/);
+    println!("too long tuple: {:?}", too_long_tuple);
+
+    let pair = (1, true);
+    println!("pair is {:?}", pair);
+
+    println!("the reversed pair is {:?}", reverse(pair));
+
+    // To create one element tuples, the comma is required to tell them apart
+    // from a literal surrounded by parentheses
+    println!("one element tuple: {:?}", (5u32, ));
+    println!("just an integer: {:?}", (5u32));
+
+    //tuples can be destructured to create bindings
+    let tuple = (1, "hello", 4.5, true);
+
+    let (a, b, c, d) = tuple;
+    println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
+
+    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
+    println!("{:?}", matrix);
+}
+
+#[test]
+fn array() {
+    use std::mem;
+
+    // This function borrows a slice
+    fn analyze_slice(slice: &[i32]) {
+        println!("first element of the slice: {}", slice[0]);
+        println!("the slice has {} elements", slice.len());
+    }
+
+    // Fixed-size array (type signature is superfluous)
+    let xs: [i32; 5] = [1, 2, 3, 4, 5];
+
+    // All elements can be initialized to the same value
+    let ys: [i32; 500] = [0; 500];
+
+    // Indexing starts at 0
+    println!("first element of the array: {}", xs[0]);
+    println!("first element of the array: {}", xs.first().unwrap());
+    println!("first element of the array: {}", xs.get(0).unwrap());
+    println!("second element of the array: {}", xs[1]);
+    // `len` returns the size of the array
+    println!("array size: {}", xs.len());
+
+    // Arrays are stack allocated
+    println!("array occupies {} bytes", mem::size_of_val(&xs)); //20
+
+    // Arrays can be automatically borrowed as slices
+    println!("borrow the whole array as a slice");
+    analyze_slice(&xs);
+
+    // Slices can point to a section of an array
+    // They are of the form [starting_index..ending_index]
+    // starting_index is the first position in the slice
+    // ending_index is one more than the last position in the slice
+    println!("borrow a section of the array as a slice");
+    analyze_slice(&ys[1..4]);
+
+    // Out of bound indexing causes compile error
+    //println!("{}", xs[5]);
+}
