b293e0dd1f4e9b2ae85c031cc6e33f0eb4c3dcd2
zing <599490911@qq.com>
2020-04-27 22:36:04 +0800

    update lifetimes

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -1,7 +1,9 @@
 pub mod conversion;
 pub mod custom_types;
 pub mod flow_control;
+pub mod fn_test;
 pub mod hello_world;
+pub mod lifetime;
 pub mod match_test;
 pub mod primitives;
 pub mod scope;
