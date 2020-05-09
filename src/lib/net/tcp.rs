04da6d4c301e62159665ad1d6492cc1316ea39cf
zing <599490911@qq.com>
2020-05-09 10:33:22 +0800

    update tcp

--- src/lib/net/tcp.rs
+++ src/lib/net/tcp.rs
@@ -104,4 +104,128 @@ pub mod tcp {
             Err(err) => println!("{}", err),
         }
     }
+
+    mod single_threaded {
+        use std::fs;
+        use std::io::prelude::*;
+        use std::net::TcpListener;
+        use std::net::TcpStream;
+
+        #[test]
+        fn main() {
+            let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+            for stream in listener.incoming() {
+                let stream = stream.unwrap();
+                println!("===================new client===================");
+                handle_connection4(stream);
+            }
+        }
+
+        fn handle_connection(mut stream: TcpStream) {
+            let mut buffer = [0; 512];
+            stream.read(&mut buffer).unwrap();
+            println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+        }
+
+        fn handle_connection2(mut stream: TcpStream) {
+            let mut buffer = [0; 512];
+            stream.read(&mut buffer).unwrap();
+            println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+            let response = "HTTP/1.1 200 OK\r\n\r\n";
+            stream.write(response.as_bytes()).unwrap();
+            stream.flush().unwrap();
+        }
+
+        fn handle_connection3(mut stream: TcpStream) {
+            let mut buffer = [0; 512];
+            stream.read(&mut buffer).unwrap();
+            let contents = fs::read_to_string("src/lib/net/hello.html").unwrap();
+            let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+            stream.write(response.as_bytes()).unwrap();
+            stream.flush().unwrap();
+        }
+
+        fn handle_connection4(mut stream: TcpStream) {
+            let mut buffer = [0; 512];
+            stream.read(&mut buffer).unwrap();
+            let get = b"GET / HTTP/1.1\r\n";
+            if buffer.starts_with(get) {
+                let contents = fs::read_to_string("src/lib/net/hello.html").unwrap();
+                let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+                stream.write(response.as_bytes()).unwrap();
+                stream.flush().unwrap();
+            } else {
+                let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+                let contents = fs::read_to_string("src/lib/net/404.html").unwrap();
+                let response = format!("{}{}", status_line, contents);
+                stream.write(response.as_bytes()).unwrap();
+                stream.flush().unwrap();
+            }
+        }
+
+        fn handle_connection5(mut stream: TcpStream) {
+            let mut buffer = [0; 512];
+            stream.read(&mut buffer).unwrap();
+            let get = b"GET / HTTP/1.1\r\n";
+            let (status_line, filename) = if buffer.starts_with(get) {
+                ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+            } else {
+                ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+            };
+
+            let contents = fs::read_to_string("src/lib/net/".to_owned() + filename).unwrap();
+            let response = format!("{}{}", status_line, contents);
+            stream.write(response.as_bytes()).unwrap();
+            stream.flush().unwrap();
+        }
+    }
+
+    mod multi_threaded {
+        use std::io::{Read, Write};
+        use std::net::{TcpListener, TcpStream};
+        use std::time::Duration;
+        use std::{fs, thread};
+
+        #[test]
+        fn main() {
+            let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+            for stream in listener.incoming() {
+                let stream = stream.unwrap();
+                println!("===================new client===================");
+                handle_connection(stream);
+            }
+        }
+
+        #[test]
+        fn main2() {
+            let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+            for stream in listener.incoming() {
+                let stream = stream.unwrap();
+                thread::spawn(|| {
+                    handle_connection(stream);
+                });
+            }
+        }
+
+        fn handle_connection(mut stream: TcpStream) {
+            let mut buffer = [0; 512];
+            stream.read(&mut buffer).unwrap();
+            let get = b"GET / HTTP/1.1\r\n";
+            let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+            let (status_line, filename) = if buffer.starts_with(get) {
+                ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+            } else if buffer.starts_with(sleep) {
+                thread::sleep(Duration::from_secs(5));
+                ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+            } else {
+                ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+            };
+            let contents = fs::read_to_string("src/lib/net/".to_owned() + filename).unwrap();
+            let response = format!("{}{}", status_line, contents);
+            stream.write(response.as_bytes()).unwrap();
+            stream.flush().unwrap();
+        }
+    }
 }
